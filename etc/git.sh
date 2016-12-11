154513a01f8fbd347d00228dfb75a430b11e4720
0ta2 <mangamania5643+github@gmail.com>
2016-12-11 20:15:07 +0900

    [add] make コマンドでインストールできるよう機能追加

---
+++ etc/git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####################################
+#
+# Git setup
+#
+####################################
+
+git config --global alias.gr "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
+git config --global alias.st status
+git config --global alias.cm commit
