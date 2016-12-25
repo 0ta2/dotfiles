6aff3a1dea624adc657efe1214ecac4144020567
ghfdjk <mangamania5643+bitbucket@gmail.com>
2016-12-25 20:42:44 +0900

    [add] checkout の alias として ch を追加

--- etc/git.sh
+++ etc/git.sh
@@ -8,3 +8,4 @@
 git config --global alias.gr "log --graph --date=short --decorate=short --pretty=format:'%Cgreen%h %Creset%cd %Cblue%cn %Cred%d %Creset%s'"
 git config --global alias.st status
 git config --global alias.cm commit
+git config --global alias.ch checkout
