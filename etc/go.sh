a302cdfb983fcaba89d2f9ce73e8ba13e75058a6
Tatsuya Suzuki <mangamania5643@gmail.com>
2019-02-05 13:12:14 +0900

    [modify] イコールを3つから2つに変更

--- etc/go.sh
+++ etc/go.sh
@@ -12,8 +12,8 @@ gopackage='
 github.com/motemen/ghq
 '
 
-if [ -d "${HOME}/.go" ]; then
-  echo "===> mkdir ${HOME}/.go"
+if [ ! -d "${HOME}/.go" ]; then
+  echo "==> mkdir ${HOME}/.go/bin"
   mkdir -p ${HOME}/.go/bin
 fi
 
@@ -21,10 +21,10 @@ fi
 for go_install in ${gopackage}; do
   go_package_name=`basename ${go_install}`
   if [ `find ${HOME}/.go/bin -name ${go_package_name}` ]; then
-    echo "===> ${go_install}をインストールします。"
+    echo "==> ${go_install}をインストールします。"
     go get ${go_install}
   else
-    echo "===> ${go_install}は、すでにインストールされています。"
+    echo "==> ${go_install}は、すでにインストールされています。"
     go get ${go_install}
   fi
 done
