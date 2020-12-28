e672740d55748d2da849575b557b3e43c7852fa5
0ta2 <mangamania5643+github@gmail.com>
2020-12-28 23:37:06 +0900

    packer.nvim を自動的にインストールするように変更

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -1,3 +1,14 @@
+-- install packer.nvim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+	execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  execute 'packadd packer.nvim'
+end
+
 --Only required if you have packer in your `opt` pack
 vim.cmd [[packadd packer.nvim]]
 
