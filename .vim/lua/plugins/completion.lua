47cc212684e53c0b97493ea76afaeed73fb6e8fb
0ta2 <mangamania5643+github@gmail.com>
2021-01-29 00:16:41 +0900

    vim-lsp のほうが安定してるので戻した

--- .vim/lua/plugins/completion.lua
+++ .vim/lua/plugins/completion.lua
@@ -2,31 +2,31 @@ local use = require"packer".use
 
 -- completion engine
 local function init()
-  -- use {'prabirshrestha/asyncomplete.vim',
-  --   config = function()
-  --     require"config.asyncomplete".init()
-  --   end,
-  --   requires = {
-  --     {'prabirshrestha/asyncomplete-lsp.vim'},
-  --     {'prabirshrestha/asyncomplete-buffer.vim'},
-  --     {'prabirshrestha/asyncomplete-file.vim'},
-  --     {'prabirshrestha/asyncomplete-ultisnips.vim'},
-  --     {'wellle/tmux-complete.vim'}
-  --   }
-  -- }
+  use {'prabirshrestha/asyncomplete.vim',
+    config = function()
+      require"config.asyncomplete".init()
+    end,
+    requires = {
+      {'prabirshrestha/asyncomplete-lsp.vim'},
+      {'prabirshrestha/asyncomplete-buffer.vim'},
+      {'prabirshrestha/asyncomplete-file.vim'},
+      {'prabirshrestha/asyncomplete-ultisnips.vim'},
+      {'wellle/tmux-complete.vim'}
+    }
+  }
 
-  use {
-     "nvim-lua/completion-nvim",
-     config = function()
-       require"config.completion".init()
-     end,
-     requires = {
-       "steelsojka/completion-buffers",
-       "honza/vim-snippets",
-       "hrsh7th/vim-vsnip",
-       "hrsh7th/vim-vsnip-integ",
-     }
-   }
+  -- use {
+  --    "nvim-lua/completion-nvim",
+  --    config = function()
+  --      require"config.completion".init()
+  --    end,
+  --    requires = {
+  --      "steelsojka/completion-buffers",
+  --      "honza/vim-snippets",
+  --      "hrsh7th/vim-vsnip",
+  --      "hrsh7th/vim-vsnip-integ",
+  --    }
+  --  }
 end
 
 return {
