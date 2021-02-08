e0eb1cbc02c49230ad0c9ced55af68ccc4bc8eea
0ta2 <mangamania5643+github@gmail.com>
2021-02-08 23:32:30 +0900

    アイコン追加

--- .vim/lua/config/vista.lua
+++ .vim/lua/config/vista.lua
@@ -2,10 +2,17 @@ local function init()
   vim.g.vista_icon_indent = {"󳄀󳄂 ", "󳄁󳄂 "}
   vim.g.vista_default_executive = 'nvim_lsp'
   vim.g["vista#renderer#icons"] = {
+    ['func'] = '󿞔',
     ['function'] = '󿞔',
     ['functions'] = '󿞔',
+    ['var'] = '󿜛',
+    ['Variable'] = '󿜛',
+    ['Variables'] = '󿜛',
+    ['const'] = '󿣿',
+    ['constant'] = '󿣿',
+    ['method'] = '󿚦',
     ['class'] = '󿦩',
-    ['property'] = '󿪶'
+    ['property'] = '󿪶',
   }
 end
 
