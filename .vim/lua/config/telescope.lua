6588a0ac63ee40d2fd343dbdb29b19533fa905ac
0ta2 <mangamania5643+github@gmail.com>
2021-02-11 21:35:02 +0900

    修正

--- .vim/lua/config/telescope.lua
+++ .vim/lua/config/telescope.lua
@@ -13,7 +13,7 @@ function M.setup_settings()
             '--line-number',
             '--column',
             '--smart-case',
-            '--unrestricted'
+            '--hidden'
          }
       }
    }
