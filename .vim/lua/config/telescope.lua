2fd9f507140fd9f4cc2583b5d5c8e792e0ca070d
0ta2 <mangamania5643+github@gmail.com>
2021-02-15 22:32:58 +0900

    telescope にしてみた

--- .vim/lua/config/telescope.lua
+++ .vim/lua/config/telescope.lua
@@ -26,6 +26,8 @@ function M.setup_keymappings()
    api.nvim_set_keymap("n", leader .. "b", "<cmd>lua require('telescope.builtin').buffers{}<CR>", opts)
    api.nvim_set_keymap("n", leader .. "c", "<cmd>lua require('telescope.builtin').commands{}<CR>", opts)
    api.nvim_set_keymap("n", leader .. "r", "<cmd>lua require('telescope.builtin').live_grep{}<CR>", opts)
+   api.nvim_set_keymap("n", leader .. "l", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find{}<CR>", opts)
+   api.nvim_set_keymap("n", leader .. "l", "<cmd>lua require('telescope.builtin').lsp_code_actions{}<CR>", opts)
 end
 
 function M.init()
