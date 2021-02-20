ed44a2eee186e9404c98cf67b73ffa9da0b6a77b
0ta2 <mangamania5643+github@gmail.com>
2021-02-20 19:13:57 +0900

    colorscheme 設定

--- .vim/lua/config/colorscheme.lua
+++ .vim/lua/config/colorscheme.lua
@@ -1,8 +1,24 @@
 local M = {}
 
-function M.init()
+function M.nvcode()
   vim.g.nvcode_termcolors = 256
-  vim.cmd [[ colorscheme nvcode ]]
+  vim.cmd [[ colorscheme onedark ]]
+end
+
+function M.sonokai()
+  vim.g.sonokai_style = 'shusia'
+  vim.g.sonokai_enable_italic = 1
+  vim.g.sonokai_disable_italic_comment = 1
+  vim.cmd [[ colorscheme sonokai ]]
+end
+
+function M.moonfly()
+  vim.cmd [[ colorscheme moonfly ]]
+end
+
+function M.init()
+  -- M.sonokai()
+  M.moonfly()
 end
 
 return M
--- .vim/lua/plugins/colorschemes.lua
+++ .vim/lua/plugins/colorschemes.lua
@@ -3,17 +3,23 @@ local use = require"packer".use
 local function init()
   use {
     'christianchiarulli/nvcode-color-schemes.vim',
-    config = function()
-      require"config.colorscheme".init()
-    end
+    -- config = function()
+   --    require"config.colorscheme".init()
+    -- end
   }
 
   use {
     'sainnhe/sonokai',
+    -- config = function()
+    --   require"config.colorscheme".init()
+    -- end
+  }
+
+  use {
+    'bluz71/vim-moonfly-colors',
     config = function()
       require"config.colorscheme".init()
     end
-
   }
 end
 
