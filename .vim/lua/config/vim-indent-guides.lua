2d2c6a3c5808cd58f9c175c68578a6d4023038b7
0ta2 <mangamania5643+github@gmail.com>
2021-02-27 23:02:17 +0900

    indent_guides_exclude_filetypes の追加

--- .vim/lua/config/vim-indent-guides.lua
+++ .vim/lua/config/vim-indent-guides.lua
@@ -4,7 +4,13 @@ local function init()
   vim.g.indent_guides_guide_size = 1
   vim.cmd('hi IndentGuidesOdd  ctermbg=black')
   vim.cmd('hi IndentGuidesEven ctermbg=darkgrey')
-  vim.g.indent_guides_exclude_filetypes = {'help', 'startify'}
+  vim.g.indent_guides_exclude_filetypes = {
+    'help',
+    'packer',
+    'lspsagafinder',
+    'TelescopePrompt',
+    'moving.lua'
+  }
 end
 
 return {
