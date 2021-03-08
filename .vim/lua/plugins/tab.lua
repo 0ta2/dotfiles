493827438019594dd4198e946e39560838f53cf9
0ta2 <mangamania5643+github@gmail.com>
2021-03-09 00:07:33 +0900

    nvim-bufferline に変更

--- .vim/lua/plugins/tab.lua
+++ .vim/lua/plugins/tab.lua
@@ -1,9 +1,20 @@
 local use = require"packer".use
 
 local function init()
+  --use {
+  --  'ap/vim-buftabline'
+  --}
+
   use {
-    'ap/vim-buftabline'
+    'akinsho/nvim-bufferline.lua',
+    config = function ()
+      require'config.nvim-bufferline'.init()
+    end,
+    requires = {
+      'kyazdani42/nvim-web-devicons'
+    }
   }
+
 end
 
 return {
