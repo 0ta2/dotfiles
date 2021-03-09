7552f253f73d3bcc9fdaad3a3ea824f9dc050a3d
0ta2 <mangamania5643+github@gmail.com>
2021-03-09 22:52:31 +0900

    hop.nvimにしてみた

--- .vim/lua/config/hop.lua
+++ .vim/lua/config/hop.lua
@@ -5,6 +5,10 @@ function M.setup_settings()
 end
 
 function M.setup_keymappings()
+  local api = vim.api
+  local opts = { noremap = false, silent = true }
+
+  api.nvim_set_keymap('n', 'fs', "<cmd>lua require'hop'.hint_char1()<cr>", opts)
 end
 
 
