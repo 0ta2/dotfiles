c8d5878e28b8fa8f0647d6d8b4577d91cfad0e90
0ta2 <mangamania5643+github@gmail.com>
2021-03-18 23:10:44 +0900

    コメントアウト解除

--- .vim/lua/config/lspsaga.lua
+++ .vim/lua/config/lspsaga.lua
@@ -35,10 +35,10 @@ end
 
 function M.setup_keymappings()
   local opts = { noremap = true, silent = true }
-  -- api.nvim_set_keymap('n', 'ca', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
+  api.nvim_set_keymap('n', 'ca', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
   api.nvim_set_keymap('n', 'gh', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
-  -- api.nvim_set_keymap('n', 'K', '<cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', opts)
-  -- api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
+  api.nvim_set_keymap('n', 'K', '<cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', opts)
+  api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
   api.nvim_set_keymap('n', leader .. 'rn', '<cmd>lua require("lspsaga.rename").rename()<CR>', opts)
 end
 
