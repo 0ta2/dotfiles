f50c3c80c879d6cee3d11b0bba1ac47b527261ec
0ta2 <mangamania5643+github@gmail.com>
2021-03-27 17:26:47 +0900

    keymap 修正

--- .vim/lua/keymap.lua
+++ .vim/lua/keymap.lua
@@ -1,50 +1,53 @@
+local opts = { noremap=true, silent=true }
+local api = vim.api
+
 -- mapleader を指定
-leader = ' '
+_G.leader = ' '
 
 -- 検索後の強調表示をの解除
-vim.api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<Esc><Esc>', ':nohlsearch<CR>', opts)
 
 -- ; : を入れ替える
-vim.api.nvim_set_keymap('n', ';', ':', { noremap = true })
+api.nvim_set_keymap('n', ';', ':', opts)
 
 -- ファイル保存
-vim.api.nvim_set_keymap('n', leader .. 'w', ':w<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', leader .. 'w', ':w<CR>', opts)
 
 -- H/L で ^ と $ に移動
-vim.api.nvim_set_keymap('n', 'H', '^', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'L', '$', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'H', '^', opts)
+api.nvim_set_keymap('n', 'L', '$', opts)
 
 -- Y で末尾までコピー
-vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
+api.nvim_set_keymap('n', 'Y', 'y$', opts)
 
 -- 行が折り返し表示されている場合、表示行単位で移動
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('v', 'j', 'gj', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('v', 'k', 'gk', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', 'j', 'gj', opts)
+api.nvim_set_keymap('n', 'k', 'gk', opts)
+api.nvim_set_keymap('v', 'j', 'gj', opts)
+api.nvim_set_keymap('v', 'k', 'gk', opts)
 
 -- 画面を横に分割
-vim.api.nvim_set_keymap('n', leader .. 's', ':<C-u>sp<CR>', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('n', leader .. 'v', ':<C-u>vs<CR>', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 's', ':<C-u>sp<CR>', opts)
+api.nvim_set_keymap('n', leader .. 'v', ':<C-u>vs<CR>', opts)
 
 -- 縦幅に最大化
-vim.api.nvim_set_keymap('n', leader .. 'o', '<C-w>_<C-w>|', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 'o', '<C-w>_<C-w>|', opts)
 -- 幅を揃える
-vim.api.nvim_set_keymap('n', leader .. '=', '<C-w>=', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. '=', '<C-w>=', opts)
 
 -- Create new buffer.
-vim.api.nvim_set_keymap('n', leader .. 't', ':enew<CR>', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 't', ':enew<CR>', opts)
 -- Close current buffer.
-vim.api.nvim_set_keymap('n', leader .. 'q', ':bp <BAR> bd! #<CR>', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 'q', ':bp <BAR> bd! #<CR>', opts)
 -- Tab to switch to next open buffer.
-vim.api.nvim_set_keymap('n', leader .. 'n', ':bnext<CR>', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 'n', ':bnext<CR>', opts)
 -- Shift + Tab to switch to previous open buffer.
-vim.api.nvim_set_keymap('n', leader .. 'p', ':bprevious<CR>', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 'p', ':bprevious<CR>', opts)
 
 -- x/s で削除した際にレジスタに入らないようにする
-vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('n', 's', '"_s', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', 'x', '"_x', opts)
+api.nvim_set_keymap('n', 's', '"_s', opts)
 
 -- vi全角英数字を半角に変換
-vim.api.nvim_set_keymap('n', leader .. 'zh', 'HzjaConvert han_eisu', { noremap = true, silent = ture })
-vim.api.nvim_set_keymap('v', leader .. 'zh', 'HzjaConvert han_eisu', { noremap = true, silent = ture })
+api.nvim_set_keymap('n', leader .. 'zh', 'HzjaConvert han_eisu', opts)
+api.nvim_set_keymap('v', leader .. 'zh', 'HzjaConvert han_eisu', opts)
