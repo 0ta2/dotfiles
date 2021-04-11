e19810d9f8314caec5093fa62e3895797685c708
0ta2 <mangamania5643+github@gmail.com>
2021-04-11 16:09:24 +0900

    Neogiを導してみた

---
+++ .vim/lua/config/neogit.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.ini()
+  local neogit = require('neogit')
+
+  neogit.setup {}
+end
+
+return M
--- .vim/lua/plugins/git.lua
+++ .vim/lua/plugins/git.lua
@@ -2,6 +2,12 @@ local use = require"packer".use
 
 local function init()
   use {
+    'TimUntersberger/neogit',
+    config = function ()
+      require"config.neogit".init()
+    end
+  }
+  use {
     'tpope/vim-fugitive'
   }
   use {
