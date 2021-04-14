918be5239d87db6e7f9347bc6ffa15fbd019755f
0ta2 <mangamania5643+github@gmail.com>
2021-04-14 22:29:38 +0900

    やっぱり必要だった

--- .vim/lua/plugins/language.lua
+++ .vim/lua/plugins/language.lua
@@ -1,11 +1,11 @@
 local use = require"packer".use
 
 local function init()
-  --use {
-  --  -- 各種言語の設定集
-  --  'sheerun/vim-polyglot'
-  --}
-  --
+  use {
+    -- 各種言語の設定集
+    'sheerun/vim-polyglot'
+  }
+
   use {
     'evidens/vim-twig'
   }
