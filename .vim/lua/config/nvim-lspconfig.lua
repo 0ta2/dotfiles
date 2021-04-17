a7d27d76d07ae476d763d4a3f7a54bbb138b7088
0ta2 <mangamania5643+github@gmail.com>
2021-04-17 17:51:41 +0900

    READMEを参考に更新

--- .vim/lua/config/nvim-lspconfig.lua
+++ .vim/lua/config/nvim-lspconfig.lua
@@ -86,6 +86,13 @@ local servers = {
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  }
+}
 
 for server, config in pairs(servers) do
   config.on_attach = on_attach
