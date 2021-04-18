c4abbbf9540558a6a925e8775abbc6e42cc0d3af
0ta2 <mangamania5643+github@gmail.com>
2021-04-18 18:00:07 +0900

    wilderを追加してみた

---
+++ .vim/lua/config/wilder.lua
@@ -0,0 +1,19 @@
+local api = vim.api
+
+local M = {}
+
+function M.init()
+  local opts = { expr = true, noremap = false, silent = true }
+
+  vim.fn['wilder#enable_cmdline_enter']()
+
+  -- vim.o.wildcharm = 9
+  vim.cmd [[ set wildcharm=<Tab> ]]
+
+  api.nvim_set_keymap('c', '<Tab>', 'wilder#in_context() ? wilder#next() : "<Tab>"', opts)
+  api.nvim_set_keymap('c', '<S-Tab>', 'wilder#in_context() ? wilder#previous() : "<S-Tab>"', opts)
+
+  vim.fn['wilder#set_option']('modes', {'/', '?', ':'})
+end
+
+return M
