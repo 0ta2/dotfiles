d4c50fb500aace0eab1a8804b6f74f8febb77c74
0ta2 <mangamania5643+github@gmail.com>
2021-04-25 11:20:47 +0900

    gh のほうが使いやすかった

--- .vim/lua/config/lspsaga.lua
+++ .vim/lua/config/lspsaga.lua
@@ -11,7 +11,7 @@ end
 
 function M.setup_keymappings()
   local opts = { noremap = true, silent = true }
-  api.nvim_set_keymap('n', 'gr', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
+  api.nvim_set_keymap('n', 'gh', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
   api.nvim_set_keymap('n', 'ca', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
   api.nvim_set_keymap('n', 'K', '<cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', opts)
   api.nvim_set_keymap('n', '<C-k>', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
