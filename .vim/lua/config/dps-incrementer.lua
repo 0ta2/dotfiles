2fb97ab799e3e3f40464610cd355464bf570bd25
0ta2 <mangamania5643+github@gmail.com>
2021-06-27 23:29:48 +0900

    自分で作ったやつを追加

---
+++ .vim/lua/config/dps-incrementer.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+function M.setup_settings()
+  local api = vim.api
+  local opts = { noremap = true, silent = false }
+  api.nvim_set_keymap('n', '+', ':Incrementer<CR>', opts)
+  api.nvim_set_keymap('n', '+', ':Decrement<CR>', opts)
+end
+
+function M.init()
+  M.setup_settings()
+end
+
+return M
