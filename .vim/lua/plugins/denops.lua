d86938a9b15caba99df484074ba416883e4aa7fc
0ta2 <mangamania5643+github@gmail.com>
2021-06-27 23:29:48 +0900

    自分で作ったやつを追加

--- .vim/lua/plugins/denops.lua
+++ .vim/lua/plugins/denops.lua
@@ -1,8 +1,16 @@
-local use = require"packer".use
+local use = require("packer").use
 
 local function init()
   use {
-    "vim-denops/denops.vim"
+    "vim-denops/denops.vim",
+    requires = {
+      {
+        '0ta2/dps-incrementer',
+        config = function()
+          require("config.dps-incrementer").init()
+        end
+      }
+    }
   }
 end
 
