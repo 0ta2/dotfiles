4c2706abcc1d7d6c0d03080d5c8004bc9b823f13
0ta2 <mangamania5643+github@gmail.com>
2021-06-28 23:09:36 +0900

    間違い修正

--- .vim/lua/config/dps-incrementer.lua
+++ .vim/lua/config/dps-incrementer.lua
@@ -4,7 +4,7 @@ function M.setup_settings()
   local api = vim.api
   local opts = { noremap = true, silent = false }
   api.nvim_set_keymap('n', '+', ':Incrementer<CR>', opts)
-  api.nvim_set_keymap('n', '+', ':Decrement<CR>', opts)
+  api.nvim_set_keymap('n', '-', ':Decrement<CR>', opts)
 end
 
 function M.init()
