04de609c06d6627a317fac2453e29cdc4574346c
0ta2 <mangamania5643+github@gmail.com>
2021-08-01 14:04:40 +0900

    incrementer 変更に伴う修正

--- .vim/lua/config/dps-incrementer.lua
+++ .vim/lua/config/dps-incrementer.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.setup_settings()
   local api = vim.api
   local opts = { noremap = true, silent = false }
-  api.nvim_set_keymap('n', '+', ':Incrementer<CR>', opts)
-  api.nvim_set_keymap('n', '-', ':Decrement<CR>', opts)
+  api.nvim_set_keymap('n', '+', ':Incrementer increment<CR>', opts)
+  api.nvim_set_keymap('n', '-', ':Incrementer decrement<CR>', opts)
 end
 
 function M.init()
