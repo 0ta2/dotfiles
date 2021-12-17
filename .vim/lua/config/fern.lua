54c4813c63e438aebfb7315ca0d101137ff2cecb
0ta2 <mangamania5643+github@gmail.com>
2021-12-18 00:46:18 +0900

    不要なファイルを非表示にした

--- .vim/lua/config/fern.lua
+++ .vim/lua/config/fern.lua
@@ -2,7 +2,8 @@ local api = vim.api
 local opts = { noremap = true, silent = true }
 
 local function init()
-  vim.g['fern#default_hidden'] = 1,
+  vim.g['fern#default_hidden'] = 1
+  vim.g['fern#default_exclude'] = '.DS_Store'
   api.nvim_set_keymap('n', leader .. 'e', ':<C-u> Fern . -drawer -toggle -reveal=% <CR>', opts)
 end
 
