ade42aa00ec5abb37dbc980fecf40ae095129fff
0ta2 <mangamania5643+github@gmail.com>
2024-10-02 17:14:18 +0900

    add fold設定追加

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -300,6 +300,9 @@ require("lazy").setup({
                     enable = true,
                 },
             }
+            vim.wo.foldmethod = 'expr'
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo.foldlevel = 3
         end
     },
 
