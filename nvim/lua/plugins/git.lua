3438c938718dacf7a64780d56cce2c8e6a77b8b7
0ta2 <mangamania5643+github@gmail.com>
2024-10-22 22:26:27 +0900

    update

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,9 +1,10 @@
 return {
     -- lazy
-    { 'nvim-lua/plenary.nvim', lazy = true },
+    { "nvim-lua/plenary.nvim", lazy = true },
+    { "sindrets/diffview.nvim", lazy = true },
 
     {
-        'lewis6991/gitsigns.nvim',
+        "lewis6991/gitsigns.nvim",
         config = true,
     },
 
@@ -156,26 +157,9 @@ return {
                 item = { ">", "v" },
                 section = { ">", "v" },
             },
-            -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
             integrations = {
-                -- If enabled, use telescope for menu selection rather than vim.ui.select.
-                -- Allows multi-select and some things that vim.ui.select doesn't.
-                telescope = nil,
-                -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-                -- The diffview integration enables the diff popup.
-                --
-                -- Requires you to have `sindrets/diffview.nvim` installed.
+                telescope = "telescope",
                 diffview = "diffview",
-
-                -- If enabled, uses fzf-lua for menu selection. If the telescope integration
-                -- is also selected then telescope is used instead
-                -- Requires you to have `ibhagwan/fzf-lua` installed.
-                fzf_lua = nil,
-
-                -- If enabled, uses mini.pick for menu selection. If the telescope integration
-                -- is also selected then telescope is used instead
-                -- Requires you to have `echasnovski/mini.pick` installed.
-                mini_pick = nil,
             },
             sections = {
                 -- Reverting/Cherry Picking
