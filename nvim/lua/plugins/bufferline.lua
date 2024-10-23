52efbe3c2e1c7cb3aea4a2ae836a0e0ecf35637a
0ta2 <mangamania5643+github@gmail.com>
2024-10-23 16:11:50 +0900

    add bufferline

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
+    {
+        'akinsho/bufferline.nvim',
+        version = "*",
+        config = true,
+        opts = {},
+    }
+}
