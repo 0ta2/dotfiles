bcf626161c2f3b7f870e786b869fbc504552d279
0ta2 <mangamania5643+github@gmail.com>
2024-10-27 02:58:46 +0900

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,19 +10,30 @@ return {
 
     {
         "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = lsp_servers
-        },
         config = function()
-            local handlers = {}
+            require("mason-lspconfig").setup {
+                ensure_installed = lsp_servers
+            }
+
+            local handlers = {
+                -- default handler
+                function (server_name)
+                    require("lspconfig")[server_name].setup {}
+                end
+            }
+
             for _, lsp in ipairs(lsp_servers) do
-                handlers[lsp] = function()
-                    local settings = require("lsp." .. lsp)
-                    require("lspconfig")[lsp].setup(settings)
+                local file = io.open("lsp." .. lsp .. ".lua", "r")
+                if file then
+                    file:close()
+                    handlers[lsp] = function()
+                        local settings = require("lsp." .. lsp)
+                        require("lspconfig")[lsp].setup(settings)
+                    end
                 end
             end
 
-          require("mason-lspconfig").setup_handlers(handlers)
+            require("mason-lspconfig").setup_handlers(handlers)
         end,
     }
 }
