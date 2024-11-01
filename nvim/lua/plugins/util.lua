b00132f76b80bfd74c77a09bfa285d238ae8b528
0ta2 <mangamania5643+github@gmail.com>
2024-11-02 02:30:44 +0900

    add

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -1,3 +1,24 @@
 return {
-    "gko/vim-coloresque"
+    -- 画面上のカラーコードを可視化
+    { "gko/vim-coloresque" },
+
+    {
+        -- 末尾の無駄なスペースの可視化
+        "bronson/vim-trailing-whitespace",
+        config = function ()
+            vim.g.extra_whitespace_ignored_filetypes = {
+                'help',
+                'lspsagafinder',
+                'TelescopePrompt',
+                'moving.lua',
+                'HopChar',
+                'NeogitPopup',
+                'NeogitStatus',
+                'mason',
+                'alpha',
+
+            },
+        end
+    }
+
 }
