2e4d609c83a4f5888dd21ed2e926615902cd90eb
0ta2 <mangamania5643+github@gmail.com>
2024-11-04 04:41:48 +0900

    update

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,44 +1,54 @@
+local enabole_file_type = {
+    "lua",
+    "go",
+    "toml",
+    "php",
+    'javascript',
+    'html',
+    'css',
+    'terraform',
+}
+
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    ft = {
-        "lua",
-        "go",
-        "toml",
-        "php",
-        'javascript',
-        'html',
-        'css',
-        'terraform',
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = true,
+        ft = enabole_file_type,
     },
-    opts = {
-        sync_install = false,
-        ensure_installed = {
-            'lua',
-            'go',
-            'toml',
-            'php',
-            'phpdoc',
-            'javascript',
-            'html',
-            'css',
-            'terraform',
+
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        ft = enabole_file_type,
+        opts = {
+            sync_install = false,
+            ensure_installed = {
+                'lua',
+                'go',
+                'toml',
+                'php',
+                'phpdoc',
+                'javascript',
+                'html',
+                'css',
+                'terraform',
+            },
+            highlight = {
+                enable = true,
+                disable = {}
+            },
+            indent = {
+                enable = true,
+            },
+            -- vim-matchup
+            matchup = {
+                enable = true,
+            }
         },
-        highlight = {
-            enable = true,
-            disable = {}
-        },
-        indent = {
-            enable = true,
-        },
-        -- vim-matchup
-        matchup = {
-            enable = true,
-        }
-    },
-    config = function()
-        vim.wo.foldmethod = 'expr'
-        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.wo.foldlevel = 99
-    end,
+        config = function()
+            vim.wo.foldmethod = 'expr'
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo.foldlevel = 99
+        end,
+    }
 }
