ae00ff20e11a091da83e8c45d54f1e5f237ad4d7
0ta2 <mangamania5643+github@gmail.com>
2024-12-26 17:07:08 +0900

    update

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -38,6 +38,7 @@ return {
             },
             filesystem = {
                 filtered_items = {
+                    visible = true,
                     hide_dotfiles = false,
                 },
                 follow_current_file = {
